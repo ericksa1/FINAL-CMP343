6d1a9298d2147a80cdb15c8c8102d7d5f1693045
ericksa1 <132090323+ericksa1@users.noreply.github.com>
2024-12-21 03:03:52 +0000

    no

---
+++ server/src/main/java/com/fitness/tracker/WorkoutController.java
@@ -0,0 +1,9 @@
+package com.fitness.tracker;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/workouts")
+public class WorkoutController {
+  // Add workout-related endpoints here
+}
